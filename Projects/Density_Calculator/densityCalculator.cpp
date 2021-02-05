81ec916bb5e4bc6cbc66a7cddcefe932b96e85e1
Rushab Patil <rdpatil@wpi.edu>
2021-02-05 17:40:00 -0500

    minor change

--- Projects/Density_Calculator/densityCalculator.cpp
+++ Projects/Density_Calculator/densityCalculator.cpp
@@ -10,8 +10,8 @@ void main()
     float T_sl = 288.16; //Kelvin(K) - Temperature at sea level
     float a_1 = -0.0065; //Kelvin per meter(K/m) - Temperature Gradient
     float T_11 = 216.66; //Kelvin(K) - Temperature at altitude = 11 km
-    float g = 9.8; //m/s^2 - Gravittional acceleration
-    
+    float g = 9.8; //merter per second square (m/s^2) - Gravittional acceleration
+
 
     //user input
     cout << "Input the altitude in km = ";
