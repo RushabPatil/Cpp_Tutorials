43302048c119657ca934b222523796685d29bdb7
Rushab Patil <rdpatil@wpi.edu>
2021-02-05 16:19:11 -0500

    Swapping the variables problem

---
+++ Problems/swappingVariableValues.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Program for sawpping values of two variables
+    int a = 20;  
+    int b = 10;
+
+    //solution with third variable
+    int temp = a;
+    a = b;
+    b = temp;
+
+    cout << "a = " << a << "b = " << b << endl;
+
+    //solution without third variable
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    cout << "a = " << a << "b = " << b << endl;
+
+}
