bfecfe382fe64ab41cc6133b3b1dc93b9947f2ee
Rushab Patil <rdpatil@wpi.edu>
2021-02-05 17:38:46 -0500

    Atmospheric Model calculator

---
+++ Projects/Density_Calculator/densityCalculator.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+    //Calculating density(kg/m^3) for a given altitude(km)
+    float altitude, density;
+    
+    //constants
+    float T_sl = 288.16; //Kelvin(K) - Temperature at sea level
+    float a_1 = -0.0065; //Kelvin per meter(K/m) - Temperature Gradient
+    float T_11 = 216.66; //Kelvin(K) - Temperature at altitude = 11 km
+    float g = 9.8; //m/s^2 - Gravittional acceleration
+    
+
+    //user input
+    cout << "Input the altitude in km = ";
+    cin >> altitude;
+
+    //
+
+
+    if(altitude > 11)
+    {
+
+    }
+    else
+    {
+
+    }
+
+}
