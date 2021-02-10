44772060613ee37c736372440cd7fad1a72f0390
Rushab Patil <rdpatil@wpi.edu>
2021-02-10 15:52:24 -0500

    Calculator App

--- Projects/Calculator/calculator.cpp
+++ Projects/Calculator/calculator.cpp
@@ -13,7 +13,29 @@ int main()
 
     switch (operation)
     {
-        case 1: 
+    case '-':
+        cout << num1 << operation << num2 << "=" << num1 - num2;
+        break;
+    case '+':
+        cout << num1 << operation << num2 << "=" << num1 + num2;
+        break;
+    case '*':
+        cout << num1 << operation << num2 << "=" << num1 * num2;
+        break;
+    case '/':
+        cout << num1 << operation << num2 << "=" << num1 / num2;
+        break;
+    case '%':
+        bool isNum1Int, isNum2Int;
+        isNum1Int = (int)num1 == num1;
+        isNum2Int = (int)num2 == num2;
 
+        if (isNum2Int && isNum1Int)
+            cout << num1 << operation << num2 << "=" << (int)num1 % (int)num2;
+        else
+            cout << "Not valid!";
+        break;
+    default:
+        cout << "Not a valid input!" << endl;
     }
 }
