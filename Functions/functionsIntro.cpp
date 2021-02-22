2f623686920005eb8feda46347c0872ba9e8fa6f
Rushab Patil <rdpatil@wpi.edu>
2021-02-21 20:55:46 -0500

    Functions

---
+++ Functions/functionsIntro.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+//function declaration
+void function();
+
+int main()
+{
+    function();
+}
+
+// Return Type -> Void
+void function()
+{
+    cout << "Hello from function()" << endl;
+}
--- Projects/Drawing/triangles.cpp
+++ Projects/Drawing/triangles.cpp
@@ -4,5 +4,36 @@ using namespace std;
 
 void main()
 {
-    
+    //Drawing a triangle
+    int height, length;
+    cout << "Height = ";
+    cin >> height;
+
+    cout << "Length = ";
+    cin >> length;
+
+    char symbol;
+    cout << "Your Symbol = ";
+    cin >> symbol;
+
+    for (int i = 1; i <= length; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << setw(2) << symbol;
+        }
+        cout << endl;
+    }
+
+    cout << endl
+         << endl;
+
+    for (int i = length; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << setw(2) << symbol;
+        }
+        cout << endl;
+    }
 }
